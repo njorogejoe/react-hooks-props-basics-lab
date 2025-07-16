32d0b34fcb3fcdfab8a47557718e41307329a1a0
Joseph <mwanikij118@gmail.com>
2025-07-16 14:55:18 +0300

    Adds Links.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
+
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +12,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
